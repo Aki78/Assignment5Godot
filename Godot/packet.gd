4f80513fd98675035a5e4ba6170fbce5ca388e27
Aki78 <kmorooka78@gmail.com>
2023-10-13 18:10:39 +0300

    done packet.gd

--- Godot/packet.gd
+++ Godot/packet.gd
@@ -13,4 +13,19 @@ func tostring() -> String:
 	var data: String = JSON.stringify(serialize_dict)
 	return data
 
-static
+func json_to_action_payload(json_str) -> Array:
+	var new_json = JSON.new()
+	var action: String
+	var payload : Array
+	var json = new_json.parse(json_str)
+	var obj_dict: Dictionary = json.data
+	
+	
+	for key in obj_dict.keys():
+		var value = obj_dict[key]
+		if key == "a":
+			action = value
+		elif key[0] == "p":
+			var index: int = key.split_floats("p", true)[1]
+			payloads.insert(index, value)
+	return [action, payloads]
